ab5d8bf3c163db2d41ff35467114e7246f88871e
Khun <lykhun@gmail.com>
2026-02-16 11:32:21 +0100

    add exercice

---
+++ DemoUnitTests/Utils/DateUtils.cs
@@ -0,0 +1,15 @@
+﻿namespace DemoUnitTests.API.Utils
+{
+    public class DateUtils
+    {
+        public static bool Overlap(DateTime startDate1, DateTime startDate2, DateTime endDate1, DateTime endDate2)
+        {
+            // s1 2000-01-01T12:00:00 
+            // s2 2000-01-02T12:00:00
+            // e1 2000-01-01T13:00:00 
+            // e2 2000-01-03T12:00:00
+            // false
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DemoUnitTests/Utils/StringUtils.cs
@@ -0,0 +1,13 @@
+﻿namespace DemoUnitTests.API.Utils
+{
+    public class StringUtils
+    {
+        public static string ToTitle(string value)
+        {
+            // je m'appelle Khun
+            // Je M'appelle Khun
+            // mon application => Mon Application
+            throw new NotImplementedException();
+        }
+    }
+}
