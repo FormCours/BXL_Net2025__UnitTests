1dd6187d6004451caab12797ad1d5b52e3af9945
Aurelien St <aurelien.strimelle@bstorm.be>
2026-02-17 09:57:09 +0100

    Ajout des models

---
+++ DemoUnitTests/Models/Command.cs
@@ -0,0 +1,17 @@
+﻿namespace DemoUnitTests.API.Models
+{
+    public class Command
+    {
+        public class CommandLine
+        {
+            public int ProductId { get; set; }
+            public int Quantity { get; set; }
+        }
+
+        public int Id { get; set; }
+        public List<CommandLine> Lines { get; set; } = [];
+
+
+        // Info liée au client...
+    }
+}
