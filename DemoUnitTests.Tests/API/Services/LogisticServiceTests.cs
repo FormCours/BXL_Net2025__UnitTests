8184395936a551b808305f62e0f889d3cea0c6dd
Aurelien St <aurelien.strimelle@bstorm.be>
2026-02-17 11:42:02 +0100

    Test avec fake 🕵️‍♂️

---
+++ DemoUnitTests.Tests/API/Services/LogisticServiceTests.cs
@@ -0,0 +1,69 @@
+﻿using DemoUnitTests.API.Interfaces;
+using DemoUnitTests.API.Models;
+using DemoUnitTests.API.Services;
+
+namespace DemoUnitTests.Tests.API.Services
+{
+    #region Fake
+    internal class FakeOrderService : IOrderService
+    {
+        public Order CreateNewOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order GetById(int orderId)
+        {
+            if (orderId != 42) 
+                throw new ArgumentException("Bad Order !");
+
+            return new Order()
+            {
+                Id = 42,
+                IsReadyForShipping = false,
+                Lines = [
+                    new Order.OrderLine() { ProductId = 1, Quantity = 10 },
+                    new Order.OrderLine() { ProductId = 3, Quantity = 2 },
+                    new Order.OrderLine() { ProductId = 4, Quantity = 1 },
+                ]
+            };
+        }
+    }
+
+    internal class FakeStockService_With10UnitByProduct : IStockService
+    {
+        public int GetStock(int productId)
+        {
+            return 10;
+        }
+
+        public void RemoveStock(int productId, int quantity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    #endregion
+
+    public class LogisticServiceTests
+    {
+        // Exemple avec des dépendences simulée
+        [Fact]
+        public void PrepareOrder_ProductOrderedInStock_ReturnTrue()
+        {
+            // Simulation des dépendences
+            IOrderService stubOrderService = new FakeOrderService();
+            IStockService stubStockService = new FakeStockService_With10UnitByProduct();
+
+            // Arrange
+            ILogisticService logisticService = new LogisticService(stubOrderService, stubStockService);
+            int orderId = 42;
+
+            // Act
+            bool result = logisticService.PrepareOrder(orderId);
+
+            //Assert
+            Assert.True(result);
+        }
+
+    }
+}
--- DemoUnitTests/Services/LogisticService.cs
+++ DemoUnitTests/Services/LogisticService.cs
@@ -1,6 +1,27 @@
-﻿namespace DemoUnitTests.API.Services
+﻿using DemoUnitTests.API.Interfaces;
+
+namespace DemoUnitTests.API.Services
 {
-    public class LogisticService
+    public class LogisticService : ILogisticService
     {
+        private readonly IOrderService _orderService;
+        private readonly IStockService _stockService;
+
+        public LogisticService(IOrderService orderService, IStockService stockService)
+        {
+            _orderService = orderService;
+            _stockService = stockService;
+        }
+
+
+        public bool PrepareOrder(int orderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValideOrder(int orderId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
